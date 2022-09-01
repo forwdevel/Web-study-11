b474521d3126c21c9a476810fa7e68fac431dec4
ForWD <forwdevel@gmail.com>
2022-09-01 15:45:02 +0900

    BoardDAO
    Add a Method

--- src/main/java/com/saeyan/dao/BoardDAO.java
+++ src/main/java/com/saeyan/dao/BoardDAO.java
@@ -1,5 +1,15 @@
 package com.saeyan.dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.saeyan.dto.BoardVO;
+
+import util.DBManager;
+
 public class BoardDAO {
 	private BoardDAO() {
 		
@@ -11,5 +21,36 @@ public class BoardDAO {
 		return instnace;
 	}
 	
-	
+	public List<BoardVO> selectAllBoards() {
+		String sql = "select * from bboard order by num desc";
+		
+		List<BoardVO> list = new ArrayList<BoardVO>();
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = DBManager.getConnection();
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
+			
+			while(rs.next()) {
+				BoardVO bVo = new BoardVO();
+				bVo.setNum(rs.getInt("num"));
+				bVo.setName(rs.getString("name"));
+				bVo.setEmail(rs.getString("email"));
+				bVo.setPass(rs.getString("pass"));
+				bVo.setTitle(rs.getString("title"));
+				bVo.setContent(rs.getString("content"));
+				bVo.setReadcount(rs.getInt("readcount"));
+				bVo.setWritedate(rs.getTimestamp("writedate"));
+				
+				list.add(bVo);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return list;
+	}
 }
