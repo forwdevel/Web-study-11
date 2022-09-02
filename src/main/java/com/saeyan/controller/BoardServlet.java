5a49a92a4c26afef745ce0c8c05d8db41f12c3f5
ForWD <forwdevel@gmail.com>
2022-09-02 11:57:18 +0900

    Update BoardServlet.java

--- src/main/java/com/saeyan/controller/BoardServlet.java
+++ src/main/java/com/saeyan/controller/BoardServlet.java
@@ -17,35 +17,38 @@ import com.seayan.controller.action.Action;
 public class BoardServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public BoardServlet() {
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public BoardServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String command = request.getParameter("command");
 		System.out.println("BoardServlet에서 요청을 받음을 확인 : " + command);
-		
-		// command에 따라서 Action 인터페이스를 자동 구현하고 실행
+
 		ActionFactory af = ActionFactory.getInstance();
 		Action action = af.getAction(command);
-		
-		if(action != null) {
+
+		if (action != null) {
 			action.execute(request, response);
 		}
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		doGet(request, response);
 	}
-
 }
